dbb7ec4191c3ccedc397ca1092129788947e3a8c
Moses <mowhite29@hotmail.co.uk>
2025-06-23 11:57:44 +0100

    feat: Added neccessary routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import {} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
+import PasswordChange from "./components/PasswordChange";
+import Vault from "./components/Vault";
+import VerifyEmail from "./components/VerifyEmail";
 
 function App() {
   return (
     <Router>
       <Route path="/" element={<Home />} />
+      <Route path="/vault/" element={<Vault />} />
+      <Route path="/verify-email/:uidb64/:token" element={<VerifyEmail />} />
+      <Route path="/password-change-confirm/:uidb64/:token" element={<PasswordChange />} />
     </Router>
   );
 }
