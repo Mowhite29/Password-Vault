9f492c0c953e90e5fe0b7c38d6e83f16665fa45e
Moses <mowhite29@hotmail.co.uk>
2025-06-23 16:41:26 +0100

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import {} from "react";
 import HeaderBar from "./HeaderBar";
+import MenuBar from "./MenuBar";
 import "../styles/Home.scss";
 
-export default function Home() {
+export default function Home(signedIn, SignInOut) {
   return (
     <>
       <HeaderBar />
+      <MenuBar signedIn={signedIn} SignInOut={SignInOut}/>
     </>
   );
 }
