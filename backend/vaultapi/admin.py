8d8d0906dbabef6604773c962a59114e4073d02e
Moses <mowhite29@hotmail.co.uk>
2025-06-23 16:38:47 +0100

    feat: added new model

--- backend/vaultapi/admin.py
+++ backend/vaultapi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import VaultEntry
+from .models import VaultEntry, UserKeys
 
 # Register your models here.
 admin.site.register(VaultEntry)
+admin.site.register(UserKeys)
