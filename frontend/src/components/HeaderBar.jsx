dcb3887b16504ffb6e6ca227b769ae05588e0ccb
Moses <mowhite29@hotmail.co.uk>
2025-06-23 13:00:37 +0100

    feat: header bar

---
+++ frontend/src/components/HeaderBar.jsx
@@ -0,0 +1,9 @@
+import "../styles/Headerbar.scss";
+export default function HeaderBar(){
+
+    return(
+        <div className="headerContainer">
+            <p>DEMO PROJECT ONLY - DO NOT USE WITH REAL DATA. THIS APPLICATION IS A PORTFOLIO PROJECT TO DEMONSTRATE SKILLS - SEE DISCLAIMER BELOW</p>
+        </div>
+    )
+}
