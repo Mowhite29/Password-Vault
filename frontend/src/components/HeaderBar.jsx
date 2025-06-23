af6cd73a6febcb495d4922b736d4ccbbc9966824
Moses <mowhite29@hotmail.co.uk>
2025-06-23 13:01:53 +0100

    prettier formatting

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -1,9 +1,11 @@
 import "../styles/Headerbar.scss";
-export default function HeaderBar(){
-
-    return(
-        <div className="headerContainer">
-            <p>DEMO PROJECT ONLY - DO NOT USE WITH REAL DATA. THIS APPLICATION IS A PORTFOLIO PROJECT TO DEMONSTRATE SKILLS - SEE DISCLAIMER BELOW</p>
-        </div>
-    )
+export default function HeaderBar() {
+  return (
+    <div className="headerContainer">
+      <p>
+        DEMO PROJECT ONLY - DO NOT USE WITH REAL DATA. THIS APPLICATION IS A
+        PORTFOLIO PROJECT TO DEMONSTRATE SKILLS - SEE DISCLAIMER BELOW
+      </p>
+    </div>
+  );
 }
