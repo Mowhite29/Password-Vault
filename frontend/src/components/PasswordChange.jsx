5cd35fed996f68c7960de81a64f46908a9446890
Moses <mowhite29@hotmail.co.uk>
2025-06-23 11:56:23 +0100

    feat: Spaceholder for PasswordChange component

---
+++ frontend/src/components/PasswordChange.jsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+export default function PasswordChange(){
+    let { uidb64, token } = useParams
+
+    return (
+        <>
+            <p>{uidb64}{token}</p>
+        </>
+    )
+}
