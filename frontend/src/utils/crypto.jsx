df8b2b27ef805660985a2049964efec21c958cfb
Moses <mowhite29@hotmail.co.uk>
2025-06-23 10:16:53 +0100

    feat: First iteration of cryptography function

--- frontend/src/utils/crypto.jsx
+++ frontend/src/utils/crypto.jsx
@@ -1,8 +1,33 @@
 import { secretbox, randomBytes } from "tweetnacl";
-import {
-  decodeUTF8,
-  encodeUTF8,
-  encodeBase64,
-  decodeBase64
-} from "tweetnacl-util";
-import argon from 'argon2-browser';
+import argon2 from 'argon2';
+
+export function Encrypt(masterKey, password) {
+  const salt = randomBytes(16);
+  const nonce = randomBytes(24);
+
+  argon2
+    .hash(masterKey, {
+      salt: salt,
+      hashLength: 32,
+      type: argon2.argon2id
+    })
+    .then(hash => {
+      return {
+        encryptedPassword: secretbox(password, nonce, hash),
+        salt: salt,
+        nonce: nonce
+      }
+    })
+}
+
+export function Decrypt(masterKey, encryptedPassword, salt, nonce){
+  argon2
+    .hash(masterKey, {
+      salt: salt,
+      hashLength: 32,
+      type: argon2.argon2id
+    })
+    .then(hash => {
+      return secretbox.open(encryptedPassword, nonce, hash)
+    })
+}
