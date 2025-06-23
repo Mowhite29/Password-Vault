06c2767d31574c0c2ad9e13c5208e081df9920c0
Moses <mowhite29@hotmail.co.uk>
2025-06-23 17:11:04 +0100

    feat: Built key checking functions

--- frontend/src/utils/crypto.jsx
+++ frontend/src/utils/crypto.jsx
@@ -31,3 +31,57 @@ export function Decrypt(masterKey, encryptedPassword, salt, nonce) {
       return secretbox.open(encryptedPassword, nonce, hash);
     });
 }
+
+export function GenerateKeyCheck(masterKey, email) {
+  const salt1 = randomBytes(16);
+  const salt2 = randomBytes(16);
+  const nonce = randomBytes(24);
+
+  const emailHash = argon2.hash(email, {
+    salt: salt1,
+    hashLength: 32,
+    type: argon2.argon2id,
+  });
+
+  argon2
+    .hash(masterKey, {
+      salt: salt2,
+      hashLength: 32,
+      type: argon2.argon2id,
+    })
+    .then((hash) => {
+      return {
+        encryptedString: secretbox(emailHash, nonce, hash),
+        salt1: salt1,
+        salt2: salt2,
+        nonce: nonce,
+      };
+    });
+}
+
+export function KeyCheck(
+  masterKey,
+  email,
+  encryptedString,
+  salt1,
+  salt2,
+  nonce,
+) {
+  const emailHash = argon2.hash(email, {
+    salt: salt1,
+    hashLength: 32,
+    type: argon2.argon2id,
+  });
+
+  argon2
+    .hash(masterKey, {
+      salt: salt2,
+      hashLength: 32,
+      type: argon2.argon2id,
+    })
+    .then((hash) => {
+      if (secretbox.open(encryptedString, nonce, hash) === emailHash) {
+        return true;
+      }
+    });
+}
