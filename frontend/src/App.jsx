8700572c75b9bb640c209a8b3f54be1eedc5128c
Moses <mowhite29@hotmail.co.uk>
2025-06-23 16:41:24 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {} from "react";
+import { useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import PasswordChange from "./components/PasswordChange";
@@ -6,11 +6,26 @@ import Vault from "./components/Vault";
 import VerifyEmail from "./components/VerifyEmail";
 
 function App() {
+  const [signedIn, setSignedIn] = useState(false)
+  const [token, setToken] = useState('')
+
+  function SignInOut(inOut, accessToken=''){
+    if (inOut === 'in'){
+      setSignedIn(true);
+      setToken(accessToken)
+    }else {
+      setSignedIn(false);
+      setToken('')
+    }
+  }
+
+
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vault/" element={<Vault />} />
+        <Route path="/" element={<Home signedIn={signedIn} SignInOut={SignInOut} />} />
+        <Route path="/vault/" element={<Vault token={token} />} />
         <Route path="/verify-email/:uidb64/:token" element={<VerifyEmail />} />
         <Route
           path="/password-change-confirm/:uidb64/:token"
