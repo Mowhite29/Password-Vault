e4225b52e5b538a0245c4278db5de452e7c80336
Moses <mowhite29@hotmail.co.uk>
2025-06-25 18:01:05 +0100

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,17 +1,25 @@
 import {} from "react";
+import { useSelector } from "react-redux";
 import HeaderBar from "./components/HeaderBar";
 import MenuBar from "./components/MenuBar";
 import SignIn from "./components/SignIn";
 import Vault from "./components/Vault";
 import "./styles/Home.scss";
 
-export default function Home(props) {
+export default function Home() {
+  const screen = useSelector((state) => state.auth.screen);
+  console.log("screen:", screen);
+
   return (
     <>
       <HeaderBar />
-      <MenuBar {...props} />
-      <SignIn styles={{ display: screen === "signin" ? "flex" : "none" }} />
-      <Vault styles={{ display: screen === "vault" ? "flex" : "none" }} />
+      <MenuBar />
+      <div
+        className="home"
+        style={{ display: screen === "home" ? "flex" : "none" }}
+      ></div>
+      {screen === "signin" && <SignIn />}
+      {screen === "signin" && <Vault />}
     </>
   );
 }
