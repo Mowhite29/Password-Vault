ca2bb1d67a94a89c0b0d0b8e9776fdef642a3697
Moses <mowhite29@hotmail.co.uk>
2025-06-25 18:00:58 +0100

    Update MenuBar.jsx

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,32 +1,54 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { signOut, setScreen } from "../redux/authSlice";
 import { deviceType } from "detect-it";
 
 import "../styles/MenuBar.scss";
 
-export default function MenuBar({ signedIn, SignInOut, CurrentScreen }) {
+export default function MenuBar() {
   const [open, setOpen] = useState(false);
+
+  const dispatch = useDispatch();
+  const signedIn = useSelector((state) => state.auth.signedIn);
 
   function OpenMenu() {
     setOpen(() => !open);
   }
 
+  const handleSignOut = () => {
+    dispatch(signOut());
+    handleScreenChange("home");
+  };
+
+  const handleScreenChange = (newScreen) => {
+    dispatch(setScreen(newScreen));
+  };
+
   if (deviceType === "touchOnly") {
     return (
       <div className={open ? "menuContainerOpen" : "menuContainer"}>
         <div className="staticMenuBar">
-          <h1>Password Vault</h1>
+          {signedIn ? (
+            <h1>Password Vault</h1>
+          ) : (
+            <button onClick={() => handleScreenChange("home")}>
+              Password Vault
+            </button>
+          )}
           <button onClick={() => OpenMenu()}>{open ? "close" : "open"}</button>
         </div>
         <div className="mobileMenu" style={{ display: open ? "flex" : "none" }}>
           <button
             onClick={
-              signedIn ? () => SignInOut("out") : () => CurrentScreen("signin")
+              signedIn
+                ? () => handleSignOut()
+                : () => handleScreenChange("signin")
             }
           >
             {signedIn ? "Sign out" : "Sign in"}
           </button>
           <button
-            onClick={() => CurrentScreen("signin")}
+            onClick={() => handleScreenChange("signin")}
             style={{ display: signedIn ? "none" : "flex" }}
           >
             Create account
