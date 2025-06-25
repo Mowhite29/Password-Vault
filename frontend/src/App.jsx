6a4b17c2d746a2fa01d3dac8bbe025c901506367
Moses <mowhite29@hotmail.co.uk>
2025-06-25 18:00:44 +0100

    feat: Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,30 @@
-import { useState } from "react";
+import {} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Home from "./Home";
 import PasswordChange from "./components/PasswordChange";
 import Vault from "./components/Vault";
 import VerifyEmail from "./components/VerifyEmail";
+import store from "./redux/store";
 
 function App() {
-  const [screen, setScreen] = useState("home");
-  const [signedIn, setSignedIn] = useState(true);
-  const [token, setToken] = useState("");
-
-  function CurrentScreen(newScreen) {
-    setScreen(newScreen);
-  }
-
-  function SignInOut(inOut, accessToken = "") {
-    if (inOut === "in") {
-      setSignedIn(true);
-      setToken(accessToken);
-      setScreen("vault");
-    } else {
-      setSignedIn(false);
-      setToken("");
-      setScreen("home");
-    }
-  }
-
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              signedIn={signedIn}
-              screen={screen}
-              SignInOut={SignInOut}
-              CurrentScreen={CurrentScreen}
-            />
-          }
-        />
-        <Route path="/vault/" element={<Vault token={token} />} />
-        <Route path="/verify-email/:uidb64/:token" element={<VerifyEmail />} />
-        <Route
-          path="/password-change-confirm/:uidb64/:token"
-          element={<PasswordChange />}
-        />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vault/" element={<Vault />} />
+          <Route
+            path="/verify-email/:uidb64/:token/"
+            element={<VerifyEmail />}
+          />
+          <Route
+            path="/password-change-confirm/:uidb64/:token"
+            element={<PasswordChange />}
+          />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
