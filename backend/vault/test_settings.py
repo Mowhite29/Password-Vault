ca9e64a2fb2bee1f71826ac02c9605a0121a02b8
Moses <mowhite29@hotmail.co.uk>
2025-06-26 13:05:56 +0100

    Update test_settings.py

--- backend/vault/test_settings.py
+++ backend/vault/test_settings.py
@@ -6,3 +6,14 @@ LOGGING = {
         'version': 1,
         'disable_existing_loggers': True,
     }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST', 'postgres'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+     }
+}
