9de5a9837382978bc0e0e2f872d7cc90bdafc250
Moses <mowhite29@hotmail.co.uk>
2025-06-27 13:29:57 +0100

    Update useKeepBackendAwake.js

--- frontend/src/hooks/useKeepBackendAwake.js
+++ frontend/src/hooks/useKeepBackendAwake.js
@@ -1,27 +1,46 @@
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setConnect } from '../redux/connectionSlice'
 
 const useKeepBackendAwake = (intervalMinutes = 4) => {
+    const dispatch = useDispatch()
+
+    const Connection = (updown) => {
+        dispatch(setConnect(updown))
+    }
+
     useEffect(() => {
         const pingUrl = import.meta.env.VITE_BACKEND_URL + '/ping/'
         if (!pingUrl) return
 
+        const pingBackend = async () => {
+            try {
+                const response = await fetch(pingUrl)
+                if (response.ok) {
+                    Connection(true)
+                } else {
+                    Connection(false)
+                    console.log('Ping failed with status:', response.status)
+                }
+            } catch (err) {
+                Connection(false)
+                console.log('Ping error:', err)
+            }
+        }
+
         // Initial wake-up ping
-        fetch(pingUrl).catch((err) =>
-            console.log('Initial backend wake-up failed:', err)
-        )
+        pingBackend()
 
         // Set interval to keep backend awake
         const interval = setInterval(
             () => {
-                fetch(pingUrl).catch((err) =>
-                    console.log('Keep-alive ping failed:', err)
-                )
+                pingBackend()
             },
             intervalMinutes * 60 * 1000
         )
 
         return () => clearInterval(interval) // Cleanup on unmount
-    }, [intervalMinutes])
+    })
 }
 
 export default useKeepBackendAwake
