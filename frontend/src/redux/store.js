59b4f8879ac1731e9b830a7c891b41d1b5f2135b
Moses <mowhite29@hotmail.co.uk>
2025-06-27 13:29:32 +0100

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
+import connectReducer from './connectionSlice'
 
 export default configureStore({
     reducer: {
         auth: authReducer,
+        connect: connectReducer,
     },
 })
