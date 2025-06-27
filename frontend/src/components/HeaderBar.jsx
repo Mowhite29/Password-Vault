76d538919dba41b57670b8b1ce296fa056d9b362
Moses <mowhite29@hotmail.co.uk>
2025-06-27 16:10:16 +0100

    Update HeaderBar.jsx

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux'
 import '../styles/Headerbar.scss'
 export default function HeaderBar() {
+    const connected = useSelector((state) => state.connect.connected)
     return (
         <div className="headerContainer">
             <p>
                 DEMO PROJECT ONLY - DO NOT USE WITH REAL DATA. THIS APPLICATION
                 IS A PORTFOLIO PROJECT TO DEMONSTRATE SKILLS - SEE DISCLAIMER
                 BELOW
+            </p>
+            <p>
+                This application uses a backend which spins down when not used.
+                Connection status: {connected ? 'Active' : 'Loading'}
             </p>
         </div>
     )
