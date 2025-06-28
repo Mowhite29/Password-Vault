dfe5847cabfbe92627221fb447570cd2f47a3d53
Moses <mowhite29@hotmail.co.uk>
2025-06-28 15:04:33 +0100

    Update test_settings.py

--- backend/vault/test_settings.py
+++ backend/vault/test_settings.py
@@ -26,6 +26,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
+     },
+     'TEST': {
+         'NAME': 'test_db'
      }
 }
 
