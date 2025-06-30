2e268e313901b980326712434fd779e7a6053809
Moses <mowhite29@hotmail.co.uk>
2025-06-30 12:48:53 +0100

    feat: Built hook to handle logout on inactivity

---
+++ frontend/src/hooks/useInactivityLogout.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { signOut } from '../redux/authSlice'
+
+const useInactivityLogout = () => {
+    const signedIn = useSelector((state) => state.auth.signedIn)
+    const timerRef = useRef(null)
+
+    const dispatch = useDispatch()
+
+    const handleSignOut = () => {
+        dispatch(signOut())
+    }
+
+    const resetTimer = () => {
+        if (timerRef.current) clearTimeout(timerRef.current);
+        timerRef.current = setTimeout(() => {
+            if (signedIn){
+                handleSignOut()
+            }
+        }, 900000);
+    };
+
+    useEffect(() => {
+        const events = ['mousemove', 'keydown', 'click', 'scroll', 'touchstart']
+
+        const handleActivity = () => resetTimer();
+
+        events.forEach(event => window.addEventListener(event, handleActivity));
+
+        resetTimer();
+
+        return () => {
+            events.forEach(event => window.removeEventListener(event, handleActivity));
+            if (timerRef.current) clearTimeout(timerRef.current);
+        }
+    })
+
+}
+
+export default useInactivityLogout
