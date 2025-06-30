2a98a31ce230fd2d7c5bbb94540c26f853325efc
Moses <mowhite29@hotmail.co.uk>
2025-06-30 15:12:36 +0100

    feat: added redux-persist to allow page refresh without requiring login again after

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,31 @@
 import {} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store'
 import Home from './Home'
 import PasswordChange from './components/PasswordChange'
 import Vault from './components/Vault'
 import VerifyEmail from './components/VerifyEmail'
-import store from './redux/store'
 
 function App() {
     return (
         <Provider store={store}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/verify-email/:uid/:token/"
-                        element={<VerifyEmail />}
-                    />
-                    <Route
-                        path="/password-change-confirm/:uidb64/:token"
-                        element={<PasswordChange />}
-                    />
-                </Routes>
-            </Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route
+                            path="/verify-email/:uid/:token/"
+                            element={<VerifyEmail />}
+                        />
+                        <Route
+                            path="/password-change-confirm/:uidb64/:token"
+                            element={<PasswordChange />}
+                        />
+                    </Routes>
+                </Router>
+            </PersistGate>
         </Provider>
     )
 }
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authReducer from './authSlice'
 import connectReducer from './connectionSlice'
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistedAuthReducer,
         connect: connectReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 })
+
+export const persistor = persistStore(store);
