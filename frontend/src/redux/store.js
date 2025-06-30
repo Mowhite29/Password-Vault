94a46b9e479e6eb5d9b5096fded161ed4a975f83
Moses <mowhite29@hotmail.co.uk>
2025-06-30 15:18:08 +0100

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,25 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import authReducer from './authSlice'
 import connectReducer from './connectionSlice'
 
 const persistConfig = {
-  key: "root",
-  storage,
-};
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+    key: 'root',
+    storage,
+}
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 export const store = configureStore({
     reducer: {
         auth: persistedAuthReducer,
         connect: connectReducer,
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-      },
-    }),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+            },
+        }),
 })
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store)
