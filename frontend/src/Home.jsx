9fab8fd079596710a3513321a25dfcb9a6ce7fc8
Moses <mowhite29@hotmail.co.uk>
2025-06-30 12:48:55 +0100

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,12 +5,14 @@ import MenuBar from './components/MenuBar'
 import SignIn from './components/SignIn'
 import Vault from './components/Vault'
 import useKeepBackendAwake from './hooks/useKeepBackendAwake'
+import useInactivityLogout from './hooks/useInactivityLogout'
 import './styles/Home.scss'
 
 export default function Home() {
     const screen = useSelector((state) => state.auth.screen)
 
     useKeepBackendAwake()
+    useInactivityLogout()
 
     return (
         <>
