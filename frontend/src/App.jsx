5aa38531ca3b5d5b17bdf1414682c49071c2a66d
Moses <mowhite29@hotmail.co.uk>
2025-07-02 14:33:04 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
 import {} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
 import Home from './Home'
 import PasswordChange from './components/PasswordChange'
-import Vault from './components/Vault'
 import VerifyEmail from './components/VerifyEmail'
 
 function App() {
     return (
         <Provider store={store}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/verify-email/:uid/:token/"
-                        element={<VerifyEmail />}
-                    />
-                    <Route
-                        path="/password-change-confirm/:uidb64/:token"
-                        element={<PasswordChange />}
-                    />
-                </Routes>
-            </Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route
+                            path="/verify-email/:uid/:token/"
+                            element={<VerifyEmail />}
+                        />
+                        <Route
+                            path="/password-change-confirm/:uidb64/:token"
+                            element={<PasswordChange />}
+                        />
+                    </Routes>
+                </Router>
+            </PersistGate>
         </Provider>
     )
 }
