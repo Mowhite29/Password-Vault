7573bc6dac4a60b3b7aedda1327841e4e54a7555
Moses <mowhite29@hotmail.co.uk>
2025-07-03 15:29:46 +0100

    Update admin.py

--- backend/vaultapi/admin.py
+++ backend/vaultapi/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import VaultEntry, UserKeys, EmailChange
+from .models import VaultEntry, UserKeys
 
 # Register your models here.
 admin.site.register(VaultEntry)
 admin.site.register(UserKeys)
-admin.site.register(EmailChange)
