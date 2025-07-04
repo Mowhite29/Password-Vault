d31cf67dfc0aebadab9d3206698ed296ee90dcf9
Moses <mowhite29@hotmail.co.uk>
2025-07-04 11:28:14 +0100

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,12 @@
 import {} from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { setScreen } from './redux/authSlice'
 import HeaderBar from './components/HeaderBar'
 import MenuBar from './components/MenuBar'
 import SignIn from './components/SignIn'
 import Vault from './components/Vault'
 import Account from './components/Account'
+import About from './components/About'
 import useKeepBackendAwake from './hooks/useKeepBackendAwake'
 import useInactivityLogout from './hooks/useInactivityLogout'
 import useTokenTimeout from './hooks/useTokenTimeout'
@@ -13,23 +15,39 @@ import './styles/Home.scss'
 export default function Home() {
     const screen = useSelector((state) => state.auth.screen)
 
+    const dispatch = useDispatch()
+
     useKeepBackendAwake()
     useInactivityLogout()
     useTokenTimeout()
+
+    const handleScreenChange = (newScreen) => {
+        dispatch(setScreen(newScreen))
+    }
 
     return (
         <>
             <HeaderBar />
             <MenuBar />
-            <div
-                className="home"
-                style={{
-                    display: screen === 'home' ? 'flex' : 'none',
-                }}
-            ></div>
+            {screen === 'home' && (
+                <div className="home">
+                    <div className="homeContainer">
+                        <h1>Secure Your Digital Life with Ease</h1>
+                        <h2>
+                            Keep your passwords safe, organized, and
+                            accessible—anytime, anywhere.
+                        </h2>
+                        <h3>Your Passwords. Protected. Simplified. Trusted.</h3>
+                        <button onClick={() => handleScreenChange('signin')}>
+                            Get started now
+                        </button>
+                    </div>
+                </div>
+            )}
             {screen === 'signin' && <SignIn />}
             {screen === 'vault' && <Vault />}
             {screen === 'account' && <Account />}
+            {screen === 'about' && <About />}
         </>
     )
 }
