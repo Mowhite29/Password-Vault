42647bc38aa7fc40c2911c7eab43b98660675384
Moses <mowhite29@hotmail.co.uk>
2025-07-04 11:27:47 +0100

    Update SignIn.jsx

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -6,7 +6,7 @@ import {
     setUserEmail,
     setRefreshToken,
 } from '../redux/authSlice'
-import { Register, TokenObtain } from '../services/api'
+import { Register, TokenObtain, PasswordReset } from '../services/api'
 import Email from './Email'
 import '../styles/SignIn.scss'
 
@@ -20,10 +20,9 @@ export default function SignIn() {
     const [messageVisible, setMessageVisible] = useState(false)
     const [emailVisible, setEmailVisible] = useState(false)
     const [emailURL, setEmailURL] = useState('')
+    const [emailType, setEmailType] = useState('')
 
     const dispatch = useDispatch()
-
-    const emailType = 'email'
 
     const usernameInput = (e) => {
         setUsername(e.target.value)
@@ -72,6 +71,7 @@ export default function SignIn() {
             handleScreenChange('vault')
         } else {
             setPopUpMessage('Cannot sign in, check credentials')
+            setTimeout(() => setMessageVisible(false), 4000)
         }
     }
 
@@ -90,9 +90,12 @@ export default function SignIn() {
                 '/' +
                 register['token'] +
                 '/'
+            setEmailType('email')
             setEmailURL(url)
-            console.log(register['uid'], register['token'])
-            setTimeout(() => setEmailVisible(true), 4000)
+            setTimeout(() => {
+                setEmailVisible(true)
+                setMessageVisible(false)
+            }, 4000)
         } else {
             setPopUpMessage(
                 'Account creation unsuccessful, please check entered details, or if you already have an account please sign in '
@@ -100,60 +103,95 @@ export default function SignIn() {
         }
     }
 
+    async function ForgottenPassword() {
+        if (username) {
+            const response = await PasswordReset(username)
+            if (response) {
+                setPopUpMessage(
+                    'Password reset request successful, if there is an account associated with this email address, a password reset email has been sent'
+                )
+                setMessageVisible(true)
+                const url =
+                    '/password-change-confirm/' +
+                    response['uid'] +
+                    '/' +
+                    response['token'] +
+                    '/'
+                setEmailType('password')
+                setEmailURL(url)
+                setTimeout(() => setEmailVisible(true), 4000)
+            }
+        } else {
+            setPopUpMessage('Please enter email address')
+            setMessageVisible(true)
+            setTimeout(() => setMessageVisible(false), 3000)
+        }
+    }
+
     return (
         <div className="signInContainer">
-            <form className="signIn" action={() => SignIn()}>
-                <p>Sign in</p>
-                <input
-                    className="usernameInput"
-                    type="email"
-                    value={username}
-                    onChange={usernameInput}
-                    placeholder="Email address"
-                    autoComplete="email-address"
-                ></input>
-                <input
-                    className="passwordinput"
-                    type="password"
-                    value={password}
-                    onChange={passwordInput}
-                    placeholder="Password"
-                    autoComplete="current-password"
-                ></input>
-                <button className="signInButton" type="submit">
-                    Sign In
+            <div className="formContainer">
+                <form className="forms" action={() => SignIn()}>
+                    <h1>Sign in</h1>
+                    <input
+                        className="usernameInput"
+                        type="email"
+                        value={username}
+                        onChange={usernameInput}
+                        placeholder="Email address"
+                        autoComplete="email-address"
+                    ></input>
+                    <input
+                        className="passwordinput"
+                        type="password"
+                        value={password}
+                        onChange={passwordInput}
+                        placeholder="Password"
+                        autoComplete="current-password"
+                    ></input>
+                    <button className="signInButton" type="submit">
+                        Sign In
+                    </button>
+                </form>
+                <button
+                    className="forgottenButton"
+                    onClick={() => ForgottenPassword()}
+                >
+                    forgotten password
                 </button>
-            </form>
-            <form className="createAccount" action={() => CreateAccount()}>
-                <p>Create account</p>
-                <input
-                    className="usernameInput"
-                    type="email"
-                    value={newUsername}
-                    onChange={newUsernameInput}
-                    placeholder="Email address"
-                    autoComplete="email-address"
-                ></input>
-                <input
-                    className="passwordinput"
-                    type="password"
-                    value={newPassword}
-                    onChange={newPasswordInput}
-                    placeholder="Password"
-                    autoComplete="none"
-                ></input>
-                <input
-                    className="nameInput"
-                    type="text"
-                    value={name}
-                    onChange={nameInput}
-                    placeholder="Name"
-                    autoComplete="name"
-                ></input>
-                <button className="createAccountButton" type="submit">
-                    Create Account
-                </button>
-            </form>
+            </div>
+            <div className="formContainer">
+                <form className="forms" action={() => CreateAccount()}>
+                    <h1>Create account</h1>
+                    <input
+                        className="usernameInput"
+                        type="email"
+                        value={newUsername}
+                        onChange={newUsernameInput}
+                        placeholder="Email address"
+                        autoComplete="email-address"
+                    ></input>
+                    <input
+                        className="passwordinput"
+                        type="password"
+                        value={newPassword}
+                        onChange={newPasswordInput}
+                        placeholder="Password"
+                        autoComplete="none"
+                    ></input>
+                    <input
+                        className="nameInput"
+                        type="text"
+                        value={name}
+                        onChange={nameInput}
+                        placeholder="Name"
+                        autoComplete="name"
+                    ></input>
+                    <button className="createAccountButton" type="submit">
+                        Create Account
+                    </button>
+                </form>
+            </div>
             {messageVisible && (
                 <div className="popUpContainer">
                     <h1>{popUpMessage}</h1>
@@ -164,7 +202,7 @@ export default function SignIn() {
                     type={emailType}
                     url={emailURL}
                     user={name}
-                    email={newUsername}
+                    email={username || newUsername}
                 />
             )}
         </div>
