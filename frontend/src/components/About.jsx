f9b43a7900a0cd7571d87b90e36e5e3878d4dd47
Moses <mowhite29@hotmail.co.uk>
2025-07-16 14:56:51 +0100

    feat: Added favicons. Added dark mode/light mode images to directory

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -125,6 +125,12 @@ export default function About() {
                     >
                         Close icons created by joalfa - Flaticon
                     </a>
+                    <a
+                        href="https://www.flaticon.com/free-icons/user"
+                        title="user icons"
+                    >
+                        User icons created by Freepik - Flaticon
+                    </a>
                 </div>
             )}
         </div>
