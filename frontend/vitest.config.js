7dbd284fab03b1208e042a7e1db3b9c498351a23
Moses <mowhite29@hotmail.co.uk>
2025-07-18 16:48:52 +0100

    Update vitest.config.js

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -13,9 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // optional
-      'github.png': path.resolve(__dirname, '__mocks__/fileMock.js'),
-      'copy.png': path.resolve(__dirname, '__mocks__/fileMock.js'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
