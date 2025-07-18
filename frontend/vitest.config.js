db8b245be8c61cc20bbb5d3213f69efe52f7c2f0
Moses <mowhite29@hotmail.co.uk>
2025-07-18 16:23:23 +0100

    Update vitest.config.js

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 // vitest.config.js
 import { defineConfig } from 'vitest/config'
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -9,6 +11,14 @@ export default defineConfig({
     include: ['test/**/*.test.{js,jsx,ts,tsx}'],
     testTimeout: 10000,
     mockReset: true,
+  },
+  resolve: {
+    alias: {
+      '\\.png$': path.resolve(__dirname, '__mocks__/fileMock.js'),
+      '\\.jpg$': path.resolve(__dirname, '__mocks__/fileMock.js'),
+      '\\.jpeg$': path.resolve(__dirname, '__mocks__/fileMock.js'),
+      '\\.svg$': path.resolve(__dirname, '__mocks__/fileMock.js'),
+    },
   },
 })
 
