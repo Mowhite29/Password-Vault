0b75148a646496c102b962dca280071c74f204ab
Moses <mowhite29@hotmail.co.uk>
2025-08-01 12:42:46 +0100

    [frontend-release]

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from "vite-plugin-top-level-await"
-import csp from "vite-plugin-csp-guard"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,18 +9,6 @@ export default defineConfig({
         react(), 
         wasm(),
         topLevelAwait(),
-        csp({
-          dev: {
-            run: false,
-            outlierSupport: ["scss"], 
-          },   
-          policy: {
-            "script-src": ["'self'", "https://www.google-analytics.com"],
-          },
-          build:{
-            sri: true
-          }
-        }),
     ],
     build: {
         outDir: '/home/runner/work/_temp/_github_home/build',
