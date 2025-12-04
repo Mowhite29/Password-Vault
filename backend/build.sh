935126ec6cf8ed854a3e571b4a730da9cc4c5901
Moses <mowhite29@hotmail.co.uk>
2025-12-04 13:11:18 +0000

    vercel deploy attempt 2

--- backend/build.sh
+++ backend/build.sh
@@ -9,4 +9,4 @@ python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.12 manage.py collectstatic --noinput --clearPP
+python3.12 manage.py collectstatic --noinput
