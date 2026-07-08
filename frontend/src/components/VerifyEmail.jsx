cd2c17592dda22549c2300d0caaf5551dcc669f1
Moses <mowhite29@hotmail.co.uk>
2026-07-08 12:21:01 +0100

    lint

--- frontend/src/components/VerifyEmail.jsx
+++ frontend/src/components/VerifyEmail.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import { useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { VerifyEmailAddress } from '../services/api'
 import { setScreen } from '../redux/authSlice'
